915c66bd18d58371c08c990d7f804ecc0e69cd2c
Alec Eastman <alec.eastman@hvcapital.com>
2023-05-09 12:41:06 +0200

    first bits of code down

---
+++ app/main.py
@@ -0,0 +1,69 @@
+# imports
+from secrets import mongo_user, mongo_pass
+from datetime import datetime
+import pyperclip as ppc
+import pymongo
+import time
+
+# mongo db inits
+uri = "mongodb+srv://" + mongo_user + ":" + mongo_pass + "@cluster0.mxftu2r.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri, server_api = pymongo.server_api.ServerApi('1'))
+mydb = client["Cluster0"]
+mycol = mydb["clips"]
+
+# send contents of clipboard to database
+def databasePaste():
+    try:
+        new_clip = {"date": f"{datetime.now()}", "contents": f"{ppc.paste()}"}
+        x = mycol.insert_one(new_clip)
+        print(x.inserted_id)
+    except Exception as e:
+        print(e)
+
+# get contents of clipboard from database
+def databaseCopy():
+    try:
+        x = mycol.find_one(sort=[("_id", pymongo.DESCENDING)])
+        ppc.copy(x["contents"])
+    except Exception as e:
+        print(e)
+
+# check if local clipboard is different from database clipboard
+def isLocalSame():
+    try:
+        x = mycol.find_one(sort=[("_id", pymongo.DESCENDING)])
+        database_contents = x["contents"]
+        local_contents = ppc.paste()
+        if (local_contents == database_contents):
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+
+def isLocalinDatabase():
+    try:
+        query = {"contents": ppc.paste()}
+        count = mycol.count_documents(query)
+        print(count)
+        if (count > 0):
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+
+def main():
+    while True:
+        if isLocalSame() is False:
+            if isLocalinDatabase() is True:
+                databaseCopy()
+                print("recieved database clipboard")
+            else:
+                databasePaste()
+                print("sent local clipboard to database")
+        time.sleep(6)
+        print("slept 6")
+
+if __name__ == "__main__":
+    main()
